c360f01fccef00f38eea39cf81a172da8f7aa12d
Barbara Czyz <bczyz@box.com>
2022-04-06 19:34:52 +0200

    Tutorial_3

---
+++ src/components/greeting.js
@@ -0,0 +1,9 @@
+// Defining the <Greeting> component
+import * as React from 'react';
+
+const Greeting = (props) => {
+    return (
+      <p>Hi, it's {props.name}!</p>
+    )
+  }
+export default Greeting;
